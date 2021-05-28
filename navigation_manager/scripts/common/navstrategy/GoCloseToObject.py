5c4714e50c6e4f6479b3694168a44da53775e1e7
Snoke13 <florian.dupuis@cpe.fr>
2021-05-28 10:49:33 +0200

    quaternion fixes

--- navigation_manager/scripts/common/navstrategy/GoCloseToObject.py
+++ navigation_manager/scripts/common/navstrategy/GoCloseToObject.py
@@ -124,9 +124,9 @@ class GoCloseToObject(GoCleanRetryReplayLastNavStrategy, object):
         targetedPose.position.x = target.position.x + r*math.sin(2*3.14*point/self.POINTS_PER_CIRCLE)
         targetedPose.position.y = target.position.y + r*math.cos(2*3.14*point/self.POINTS_PER_CIRCLE)
 
-        rotationOffset = tf.transformations.quaternion_from_euler(-0.35, 0, 0)
-        targetedPose.orientation = MathToolbox.computeQuaternion(
-            targetedPose.position.x, targetedPose.position.y, target.position.x, target.position.y) * rotationOffset
+        quaternion = tf.transformations.quaternion_from_euler(0, 0, math.atan2(
+        float(target.position.y-targetedPose.position.y), float(target.position.x-targetedPose.position.x)) - 0.35)
+        targetedPose.orientation = quaternion
 
         return targetedPose
 
